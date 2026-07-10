016115d83713bfebc99fac5114eb9b0e60b37f71
abcd567a <28452511+abcd567a@users.noreply.github.com>
2026-07-10 11:55:18 -0400

    added support for ubuntu 26 resolute
    Updated OS version checks and added support for Ubuntu 24 (resolute). Enhanced architecture handling for arm64.

--- install-rb24-ubuntu-amd64.sh
+++ install-rb24-ubuntu-amd64.sh
@@ -6,17 +6,21 @@ OS_ID=`lsb_release -si`
 OS_DESCRIPTION=`lsb_release -sd`
 OS_ARCH=`dpkg --print-architecture`
 
-if [[ ${OS_VERSION} == noble && ${OS_ARCH} == amd64 ]]; then
-  echo -e "\e[1;32mDetected OS" ${OS_ID} ${OS_DESCRIPTION} ${OS_ARCH}"\e[0;39m"
+if [[ ${OS_VERSION} == resolute && ${OS_ARCH} == amd64 ]]; then
+  echo -e "\e[1;32mDetected OS" ${OS_DESCRIPTION}  $OS_VERSION ${OS_ARCH}"\e[0;39m"
+  echo -e "\e[1;32mProceeding with installation ...\e[0;39m"; sleep 2
+
+elif [[ ${OS_VERSION} == noble && ${OS_ARCH} == amd64 ]]; then
+  echo -e "\e[1;32mDetected OS" ${OS_DESCRIPTION}  $OS_VERSION ${OS_ARCH}"\e[0;39m"
   echo -e "\e[1;32mProceeding with installation ...\e[0;39m"; sleep 2
 
 elif [[ ${OS_VERSION} == jammy && ${OS_ARCH} == amd64 ]]; then
-  echo -e "\e[1;32mDetected OS" ${OS_DESCRIPTION} ${OS_ARCH}"\e[0;39m"
+  echo -e "\e[1;32mDetected OS" ${OS_DESCRIPTION} $OS_VERSION ${OS_ARCH}"\e[0;39m"
   echo -e "\e[1;32mProceeding with installation ...\e[0;39m"; sleep 2
 
 else
-  echo -e "\e[1;32mDetected your OS is:" ${OS_ID} ${OS_DESCRIPTION} ${OS_ARCH}"\e[0;39m"
-  echo -e "\e[1;32mThis script is only for Ubuntu 22 & 24 amd64 (jammy & noble) \e[0;39m"
+  echo -e "\e[1;32mDetected your OS is:"${OS_DESCRIPTION} $OS_VERSION ${OS_ARCH}"\e[0;39m"
+  echo -e "\e[1;32mThis script is only for Ubuntu 22, 24, & 26 amd64 (jammy, noble, & resolute) \e[0;39m"
   echo -e "\e[1;32mIt will abort now...\e[0;39m"
   exit 0
 fi
@@ -36,8 +40,30 @@ apt -y install qemu-user qemu-user-binfmt binfmt-support
 echo -e "\e[1;32mAdding arhitecture arm64 to system ...\e[0;39m"; sleep 2
 dpkg --add-architecture arm64
 
+if [[ ${OS_VERSION} == resolute ]]; then
+cp -n /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list.d/ubuntu.sources.bak
+echo -e "\e[1;32mAdding line \"Architectures: amd64\" to existing file \"ubuntu.sources\", if not already there ...\e[0;39m"; sleep 2
+sed -i '/Architectures: amd64/d' /etc/apt/sources.list.d/ubuntu.sources
+sed -i '/Types: deb/a Architectures: amd64' /etc/apt/sources.list.d/ubuntu.sources
 
-if [[ ${OS_VERSION} == noble ]]; then
+echo -e "\e[1;32mCreating arm64 apt-source file \"ubuntu-ports-arm64.sources\"  \e[0;39m"; sleep 2
+RESOLUTE_ARM64_SOURCES_FILE=/etc/apt/sources.list.d/ubuntu-ports-arm64.sources
+touch ${RESOLUTE_ARM64_SOURCES_FILE}
+chmod 777 ${RESOLUTE_ARM64_SOURCES_FILE}
+echo "Writing code to file ubuntu-ports-arm64.sources"
+/bin/cat <<EOM >${RESOLUTE_ARM64_SOURCES_FILE}
+Types: deb
+URIs: http://ports.ubuntu.com/ubuntu-ports/
+Suites: resolute resolute-updates resolute-security
+Components: main restricted universe multiverse
+Architectures: arm64
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+
+EOM
+
+chmod 644 ${RESOLUTE_ARM64_SOURCES_FILE}
+
+elif [[ ${OS_VERSION} == noble ]]; then
 cp -n /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list.d/ubuntu.sources.bak
 echo -e "\e[1;32mAdding line \"Architectures: amd64\" to existing file \"ubuntu.sources\", if not already there ...\e[0;39m"; sleep 2
 sed -i '/Architectures: amd64/d' /etc/apt/sources.list.d/ubuntu.sources
@@ -99,7 +125,7 @@ apt install -y dirmngr gnupg
 gpg --keyserver keyserver.ubuntu.com --recv-keys F2A8428D3C354953
 gpg --export --armor F2A8428D3C354953 | sudo gpg --dearmor -o /etc/apt/keyrings/rb24.gpg
 
-if [[ ${OS_VERSION} == noble ]]; then
+if [[ ${OS_VERSION} == noble || ${OS_VERSION} == resolute ]]; then
 echo "deb [signed-by=/etc/apt/keyrings/rb24.gpg] https://apt.rb24.com/ bookworm main" | sudo tee /etc/apt/sources.list.d/rb24.list
 elif [[ ${OS_VERSION} == jammy ]]; then
 echo "deb [signed-by=/etc/apt/keyrings/rb24.gpg] https://apt.rb24.com/ bullseye main" | sudo tee /etc/apt/sources.list.d/rb24.list
